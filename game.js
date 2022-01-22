0d076db155d38c8006ceef074bb687f9eecf41aa
MariamElZaatari <mariamalzaatari@gmail.com>
2022-01-22 21:59:35 +0200

    Status You Lose added

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
 function init() {
     var start = document.getElementById("start");
     var boundaries = document.getElementsByClassName("boundary");
-
-    start.onclick = function () {
-        reset();
+    var status=document.getElementById("status");
+    var firstTry=true;
+    var counter=0;
+    start.onmouseenter = function () {
+        if (firstTry){
+            status.innerHTML="<br/>";
+        }
+        firstTry=false;
         for (var i = 0; i < boundaries.length; i++) {
             boundaries[i].onmouseover = function () {
-                for (var i = 0; i < boundaries.length; i++)
+                for (var i = 0; i < boundaries.length; i++){
                     boundaries[i].style.backgroundColor = "red";
-            }
+                }
+                status.innerHTML="Loser"
+            };
         }
-    }
-    function reset() {
-        for (var i = 0; i < boundaries.length; i++)
+    };
+    start.onclick = function reset() {
+        for (var i = 0; i < boundaries.length; i++){
             boundaries[i].style.backgroundColor = "#eeeeee";
-    }
-}
-
-
+        }
+        status.innerHTML="<br/>";
+    };
+};
 document.addEventListener("DOMContentLoaded", () => init());
 
