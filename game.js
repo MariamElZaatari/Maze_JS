1a77103092cbd656546042e036878dc2b783f606
MariamElZaatari <mariamalzaatari@gmail.com>
2022-01-22 23:53:27 +0200

    score and reset functioning

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 function init() {
-
+    
+    var boundaries = document.getElementsByClassName("boundary");
     var score = 0;
+    var startGame=false;
+    var firstTry=true;
 
     document.getElementsByClassName("example")[0].style.textAlign = "center";
     document.getElementsByClassName("example")[0].innerHTML = score;
@@ -9,28 +12,49 @@ function init() {
     resetBtn.innerHTML = "reset";
     resetBtn.onclick = function(){
         score = 0;
+        document.getElementsByClassName("example")[0].innerHTML = score;
+        for (var i = 0; i < boundaries.length; i++){
+            boundaries[i].style.backgroundColor = "#eeeeee";
+        }
+        status.innerHTML='Begin by moving your mouse over the "S".';
+        startGame = false;
+
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].onmouseover = function () {
+            };
+        }
+        firstTry = true;
     }
 
     document.body.append(resetBtn)
 
     var start = document.getElementById("start");
-    var boundaries = document.getElementsByClassName("boundary");
     var status=document.getElementById("status");
-    var firstTry=true;
-    var counter=0;
+    
     start.onmouseenter = function () {
+        startGame=true;
         if (firstTry){
             status.innerHTML="<br/>";
         }
         firstTry=false;
         for (var i = 0; i < boundaries.length; i++) {
+            if(boundaries[i].classList.contains("example"))
+                continue
             boundaries[i].onmouseover = function () {
                 for (var i = 0; i < boundaries.length; i++){
+                    if(boundaries[i].classList.contains("example"))
+                    continue
                     boundaries[i].style.backgroundColor = "red";
                 }
+                if(status.innerHTML!="You Lose"){
+                    score = score - 10;
+                    document.getElementsByClassName("example")[0].innerHTML = score;
+                }
                 status.innerHTML="You Lose"
+
             };
         }
+
     };
     start.onclick = function reset() {
         for (var i = 0; i < boundaries.length; i++){
@@ -41,11 +65,16 @@ function init() {
 
     var end=document.getElementById("end");
     end.onmouseenter = function(){
+        if(!startGame)
+            return
         var boundary=document.getElementById("boundary1");
         if(boundary.style.backgroundColor!="red"){
-            status.innerHTML="You Win";
-
             
+            if(status.innerHTML != "You Win"){
+                score = score + 5;
+                document.getElementsByClassName("example")[0].innerHTML = score;
+            }
+            status.innerHTML="You Win";
 
             for (var i = 0; i < boundaries.length; i++) {
                 boundaries[i].onmouseover = function () {
